b759373cf36399bcc221f95c24760d659c34b685
vedansh1758 <99556966+vedansh1758@users.noreply.github.com>
2022-02-24 17:21:52 +0530

    Add files via upload

---
+++ vedansh5.py
@@ -0,0 +1,6 @@
+#Write a program to enter base and height and calculate the hypotnuese.
+base=int(input("Enter the base"))
+height=int(input("Enter the height"))
+h=((base)**2+(height)**2)**(1/2)
+print("Hypotenuse is %f"%h)
+
