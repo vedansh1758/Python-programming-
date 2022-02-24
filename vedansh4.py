055d4d0f365807e7046ecccc38b2308e4204ccd0
vedansh1758 <99556966+vedansh1758@users.noreply.github.com>
2022-02-24 17:21:24 +0530

    Add files via upload

---
+++ vedansh4.py
@@ -0,0 +1,8 @@
+#Write a program to input coordiantes and find distance between them.
+x1=int(input("Enter the x1 coordinate"))
+x2=int(input("Enter the x2 coordinate"))
+y1=int(input("Enter the y1 coordinate"))
+y2=int(input("Enter the  y2 coordinate"))
+d=((y2-y1)**2+(x2-x1)**2)**(1/2)
+print(d)
+
