a75ee28f03fc225637329732deb1fbe438ec8682
vedansh1758 <99556966+vedansh1758@users.noreply.github.com>
2022-03-03 17:42:48 +0530

    Add files via upload

--- vedansh6.py
+++ vedansh6.py
@@ -1,7 +1,21 @@
-#Write a program to find whether the number is even or odd.
-n=int(input("Enter the number"))
-b=n&1
-if(b==1):
-	print("The number is even")
-else:
-	print("The number is odd")
+"""Write a program to calculate salary of an employee with following condition
+1)if basic salary is <10000
+then HRA=20% and DA=80%
+2)if basic salary is >10000 and <=20000
+then HRA=25% and DA=85%
+1)if basic salary is >20000
+then HRA=30% and DA=95%"""
+s=int(input("Enter the basic salary of employee")
+if (s<10000):
+	hra=(20*s)/100
+	da=(80*s)/100
+	print("The salaray of emplyoee is %f"%(s+hra+da))
+elif(s>10000 and s<=20000):
+	hra=(25*s)/100
+	da=(85*s)/100
+	print("The salaray of emplyoee is %f"%(s+hra+da))
+elif(s>20000):
+	hra=(30*s)/100
+	da=(95*s)/100
+	print("The salaray of emplyoee is %f"%(s+hra+da))
+
